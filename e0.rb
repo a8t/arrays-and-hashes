9bbaaec2ca1f0243323a21c66e41b3a792b3c9e0
Andy Tran <andy8tran@gmail.com>
2017-08-10 17:07:02 -0400

    finished up til 7. fml

--- e0.rb
+++ e0.rb
@@ -124,10 +124,10 @@ cities[:hamilton] = 23
 
 # Exercise 5
 
-# 1
+# 1 .values outputs an array of the values. reduce combines the values, in this case with a + in between. prints that sum.
 puts cities.values.reduce(:+)
 
-# 2
+# 2 Ternary decides to print X is old if age > 30, etc
 friends.each  {|name, age| puts (age > 30 ? "#{name} is old." : "#{name} is young.")}
 
 # 3
@@ -138,3 +138,84 @@ ages.each {|age| puts "#{age+=1}"}
 
 # 5
 puts colors << "purple" << "black"
+
+# Exercise 6
+
+# 1
+new_movies = {
+  "The Mummy": 1999,
+  "The Matrix": 1999,
+  'Avatar': 2009,
+  'Star Trek': 2009
+}
+
+# 2
+phone = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  ["*", 0, '#']
+]
+# 3
+countries = [
+  {
+    name:      "France",
+    continent: "Europe",
+    island:    false
+  },
+  {
+    name:      "Mongolia",
+    continent: "Asia",
+    island:    false
+  },
+  {
+    name:      "South Africa",
+    continent: "Africa",
+    island:    false
+  },
+  {
+    name:      "Australia",
+    continent: "Oceania",
+    island:    true
+  },
+]
+
+# Exercise 6
+# 1
+20.times do puts 'I will not skateboard in the halls' end
+
+# 2
+skate_arr = []
+20.times do skate_arr.push('I will not skateboard in the halls') end
+
+# 3
+number_arr = (1..50).to_a
+
+# 4
+sum = 0
+number_arr.each { |number| sum+=number}
+puts sum
+
+# I would instead do it with a reduce: puts number_arr.reduce(:+)
+
+# 5
+threes_arr = []
+number_arr.each { |number| 3.times do threes_arr.push(number) end}
+puts threes_arr
+
+# 6
+not_islands = countries.select { |country| country[:island]==false}.to_a
+
+# Exercise 7
+expenses = [ 80, 100, 120, 2.3, 19.23]
+puts expenses.reduce(:+)
+
+def sum(arr)
+  arr.reduce(:+)
+end
+
+puts sum([1234234,12342134312341234])
+puts sum([1,2,3,4,5])
+
+# Exercise 8
+grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
